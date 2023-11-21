13203d7d21a8585667017959137277fadb958658
Helix <diegoatari8@gmail.com>
2023-11-21 18:42:38 -0300

    Readded files

---
+++ Components/Debug.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BardNetworking.Components
+{
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public enum LogSource
+    {
+        Client,
+        Server
+    }
+    internal class Debug
+    {
+        static ConsoleColor[] consoleColors = new ConsoleColor[] { ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Red };
+        public static void Log(string message, LogSource source, LogType type = LogType.Info)
+        {
+#if UNITY_EDITOR
+         UnityEngine.Debug.Log(message);
+         
+#else  
+            Console.ForegroundColor = consoleColors[(int)type];
+            Console.WriteLine("[Bard|" + source.ToString() + "|" + type.ToString() + "] " + message);
+            Console.ForegroundColor = ConsoleColor.White;
+#endif
+        }
+    }
+}
---
+++ Example.cs
@@ -0,0 +1,51 @@
+﻿using BardNetworking.Components;
+
+namespace BardNetworking
+{
+    internal class Example
+    {   
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+            Console.WriteLine("Bard Networking Example");
+            PacketReader reader = new PacketReader();
+            BardServer server = new BardServer(reader);
+            server.Start();
+            BardClient[] clients = new BardClient[25];
+
+                for (int i = 0; i < clients.Length; i++)
+                {
+                    clients[i] = new BardClient(reader);
+                    clients[i].Connect();
+            
+                }
+          
+                server.SendToAll("Hello, world!");
+
+        
+                for (int i = 0; i < clients.Length; i++)
+                {
+                    clients[i].Send("Hello, server!");
+
+                }
+      
+                for (int i = 0; i < clients.Length; i++)
+                {
+                    clients[i].Send("Bye, server!");
+               
+                    clients[i].Disconnect();
+                }
+            
+               
+                Debug.Log("Only " + server.clients.Count + " peeps remain.",LogSource.Server);
+                while (true)
+                {
+                    await Task.Delay(1000   );
+                 
+                }
+
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
