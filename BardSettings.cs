133781b2a15fc82ce7a0fbdfee23edb7b3d87413
Helix <diegoatari8@gmail.com>
2023-11-21 18:33:10 -0300

    Initial commit

---
+++ BardSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BardNetworking
+{
+    internal class BardSettings
+    {
+        public const int MAX_PACKET_SIZE = 1024;
+    }
+}
