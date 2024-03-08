9b4e01803b1b91a7b41c57e38309ad70c16444a1
Helix <diegoatari8@gmail.com>
2024-03-07 23:39:01 -0300

    Update PacketReader.cs
    Removed weird unity dependencies

--- Components/PacketReader.cs
+++ Components/PacketReader.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Reflection;
-using Unity.VisualScripting;
 using System.Runtime.InteropServices;
 
 namespace BardNetworking.Components
